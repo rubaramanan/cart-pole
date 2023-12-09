f14a1ea078549f3c57b4219a21c336f3a48271c0
Ramanan <rubaramanan1997@gmail.com>
2023-12-09 14:34:15 -0500

    util functions

---
+++ utils.py
@@ -0,0 +1,34 @@
+from __future__ import division, absolute_import, print_function
+
+from tf_agents.drivers import py_driver
+from tf_agents.policies import py_tf_eager_policy
+
+from train import rb_observer
+
+
+def compute_avg_return(environment, policy, num_episodes=10):
+    total_return = 0.0
+    for _ in range(num_episodes):
+
+        time_step = environment.reset()
+        episode_return = 0.0
+
+        while not time_step.is_last():
+            action_step = policy.action(time_step)
+            time_step = environment.step(action_step.action)
+            episode_return += time_step.reward
+        total_return += episode_return
+
+    avg_return = total_return / num_episodes
+    return avg_return.numpy()[0]
+
+
+def collect_episode(environment, policy, num_episodes):
+    driver = py_driver.PyDriver(
+        environment,
+        py_tf_eager_policy.PyTFEagerPolicy(
+            policy, use_tf_function=True),
+        [rb_observer],
+        max_episodes=num_episodes)
+    initial_time_step = environment.reset()
+    driver.run(initial_time_step)
