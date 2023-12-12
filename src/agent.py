614430dec43c98acb22d298ba35f5c169178e257
Ramanan <rubaramanan1997@gmail.com>
2023-12-12 17:34:58 -0500

    agent

--- src/agent.py
+++ src/agent.py
@@ -4,22 +4,22 @@ import tensorflow as tf
 from tf_agents.agents.reinforce import reinforce_agent
 from tf_agents.networks import actor_distribution_network
 
-from train import train_env, fc_layer_params, learning_rate
+from conf.config import ReinforceConfig
 
 
-def create_agent():
+def create_agent(config: ReinforceConfig):
     # agent
     actor_net = actor_distribution_network.ActorDistributionNetwork(
-        train_env.observation_spec(),
-        train_env.action_spec(),
-        fc_layer_params=fc_layer_params)
-    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+        config.train_env.observation_spec(),
+        config.train_env.action_spec(),
+        fc_layer_params=config.fc_layer_params)
+    optimizer = tf.keras.optimizers.Adam(learning_rate=config.learning_rate)
     train_step_counter = tf.Variable(0)
-    tf_agent = reinforce_agent.ReinforceAgent(
-        train_env.time_step_spec(),
-        train_env.action_spec(),
+    config.tf_agent = reinforce_agent.ReinforceAgent(
+        config.train_env.time_step_spec(),
+        config.train_env.action_spec(),
         actor_network=actor_net,
         optimizer=optimizer,
         normalize_returns=True,
         train_step_counter=train_step_counter)
-    tf_agent.initialize()
+    config.tf_agent.initialize()
