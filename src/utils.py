d438f2235974fa170dc409f40480295796709c04
Ramanan <rubaramanan1997@gmail.com>
2023-12-12 17:34:34 -0500

    util functions

--- src/utils.py
+++ src/utils.py
@@ -3,10 +3,12 @@ from __future__ import division, absolute_import, print_function
 import base64
 
 import IPython
+import imageio
+from matplotlib import pyplot as plt
 from tf_agents.drivers import py_driver
 from tf_agents.policies import py_tf_eager_policy
 
-from train import rb_observer
+from conf.config import ReinforceConfig
 
 
 def compute_avg_return(environment, policy, num_episodes=10):
@@ -26,14 +28,14 @@ def compute_avg_return(environment, policy, num_episodes=10):
     return avg_return.numpy()[0]
 
 
-def collect_episode(environment, policy, num_episodes):
+def collect_episode(config: ReinforceConfig):
     driver = py_driver.PyDriver(
-        environment,
+        config.train_py_env,
         py_tf_eager_policy.PyTFEagerPolicy(
-            policy, use_tf_function=True),
-        [rb_observer],
-        max_episodes=num_episodes)
-    initial_time_step = environment.reset()
+            config.tf_agent.collect_policy, use_tf_function=True),
+        [config.rb_observer],
+        max_episodes=config.collect_episodes_per_iteration)
+    initial_time_step = config.train_py_env.reset()
     driver.run(initial_time_step)
 
 
@@ -48,3 +50,22 @@ def embed_mp4(filename):
   </video>'''.format(b64.decode())
 
     return IPython.display.HTML(tag)
+
+
+def write_evaluation_video(config: ReinforceConfig):
+    with imageio.get_writer(config.video_filename, format='FFMPEG', fps=60) as video:
+        for _ in range(config.num_episodes):
+            time_step = config.eval_env.reset()
+            video.append_data(config.eval_py_env.render())
+            while not time_step.is_last():
+                action_step = config.tf_agent.policy.action(time_step)
+                time_step = config.eval_env.step(action_step.action)
+                video.append_data(config.eval_py_env.render())
+
+
+def plot_agent_step(config: ReinforceConfig, returns):
+    steps = range(0, config.num_iterations + 1, config.eval_interval)
+    plt.plot(steps, returns)
+    plt.ylabel('Average Return')
+    plt.xlabel('Step')
+    plt.ylim(top=250)
